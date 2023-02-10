97a2db62bdd4820f96c70e1c4dfde5398e14b654
Kian <fafrd@users.noreply.github.com>
2023-02-09 21:00:45 -0800

    cgroups2 total mem fix

--- eth/ethconfig/estimate/esitmated_ram.go
+++ eth/ethconfig/estimate/esitmated_ram.go
@@ -1,7 +1,10 @@
 package estimate
 
 import (
+	"bufio"
+	"os"
 	"runtime"
+	"strconv"
 
 	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/erigon-lib/common/cmp"
@@ -12,8 +15,15 @@ type estimatedRamPerWorker datasize.ByteSize
 
 // Workers - return max workers amount based on total Memory/CPU's and estimated RAM per worker
 func (r estimatedRamPerWorker) Workers() int {
+	// first check if we are running under cgroup limits (docker, kubernetes, etc)
+	maxMemory, err := fetchCgroupMemoryMax()
+	if err != nil {
+		// if error, fall back to total memory
+		maxMemory = memory.TotalMemory()
+	}
+
 	// 50% of TotalMemory. Better don't count on 100% because OOM Killer may have aggressive defaults and other software may need RAM
-	maxWorkersForGivenMemory := (memory.TotalMemory() / 2) / uint64(r)
+	maxWorkersForGivenMemory := (maxMemory / 2) / uint64(r)
 	return cmp.Min(AlmostAllCPUs(), int(maxWorkersForGivenMemory))
 }
 func (r estimatedRamPerWorker) WorkersHalf() int    { return cmp.Max(1, r.Workers()/2) }
@@ -29,4 +39,22 @@ const (
 // user can reduce GOMAXPROCS env variable
 func AlmostAllCPUs() int {
 	return cmp.Max(1, runtime.GOMAXPROCS(-1)-1)
+}
+
+func fetchCgroupMemoryMax() (uint64, error) {
+	file, err := os.Open("/sys/fs/cgroup/memory.max")
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		memory, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return 0, err
+		}
+		return uint64(memory), nil
+	}
+	return 0, scanner.Err()
 }
